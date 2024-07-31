d98dd2e2473ef8b70d16a01fe1d2686d8ecaeca1
Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2024-07-31 23:37:48 +0200

    init I18n

--- components/layout/components/menu-header/menu-header.tsx
+++ components/layout/components/menu-header/menu-header.tsx
@@ -4,7 +4,7 @@ import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import React from 'react'
 
-const MenuHeader = ({session}: {session: Session}) => {
+const MenuHeader = ({session}: {session: any}) => {
   const t = useTranslations('Menu');
   return (
     <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40 justify-between lg:justify-end">
