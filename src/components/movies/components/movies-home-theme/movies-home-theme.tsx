7c733cc1e41a665af96f527848a59fff1b259c7d
Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2024-08-20 09:47:47 +0200

    fixed bug

--- src/components/movies/components/movies-home-theme/movies-home-theme.tsx
+++ src/components/movies/components/movies-home-theme/movies-home-theme.tsx
@@ -15,6 +15,16 @@ const MoviesHomeTheme = ({ movies, country, isMobileView }:{movies?:IMovie[], co
  const locale = useLocale()
  
  const responsive = {
+  tablet: {
+    breakpoint: { max: 1024, min: 465 },
+    items: 3,
+    slidesToSlide: 1 
+  },
+  tabletHorizontal: {
+    breakpoint: { max: 1400, min: 1025 },
+    items: 3,
+    slidesToSlide: 1 
+  },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 3,
@@ -24,20 +34,20 @@ const MoviesHomeTheme = ({ movies, country, isMobileView }:{movies?:IMovie[], co
 
   return ( 
     <Suspense fallback={<LoadingSpinner />}>
-      <div className={`w-full aspect-[1/0.4] relative bg-chicago bg-cover bg-center`}>
+      <div className={`w-full aspect-[0/0.4] md:aspect-[1/0.4] relative bg-chicago bg-cover bg-center`}>
         <div className='absolute w-full h-full bg-slate-950 opacity-50  top-0 left-0 z-0'></div>
           <Container className='h-full pt-6 pb-6 flex relative flex-col justify-start items-start'>
-            
             <Title className='text-2xl relative  md:text-4xl' text={country} type='h3' /> 
 
             <div className='h-full flex flex-wrap justify-start items-end gap-2'>
-            {movies && movies?.length > 0 ?
-            !isMobileView ?
+            {movies && movies?.length > 0 && !isMobileView &&
+    
               movies?.map((movie, index) => (
                 <MovieItemTheme key={index} locale={locale} movie={movie} image={movie.image}  id={movie.id} />
               ))
-            : 
-            
+            }
+            </div>
+            {movies && movies?.length > 0 && isMobileView &&
             <Carousel
               containerClass="h-48 w-full mt-6 z-99"
               autoPlay={false}
@@ -46,15 +56,12 @@ const MoviesHomeTheme = ({ movies, country, isMobileView }:{movies?:IMovie[], co
               infinite={true} 
               responsive={responsive} 
             >
-              { movies && movies?.length > 0 && movies?.map((movie, index) => (
+              {movies?.map((movie, index) => (
                   <MovieItemCarousel key={index} locale={locale} movie={movie} image={movie.image}  id={movie.id} />
                 ))}
-            </Carousel>
-            : null}
-            </div>
+            </Carousel>}
           </Container>
       </div>
-
     </Suspense>
   )
 }
