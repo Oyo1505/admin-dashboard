b200fd0faffa7ddf51f49fe8a15c7f9c6705061e
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2024-10-10 10:54:50 +0200

    cache validation

--- src/app/dashboard/(dashboard)/add-movie/page.tsx
+++ src/app/dashboard/(dashboard)/add-movie/page.tsx
@@ -2,13 +2,20 @@ import { getAllMovies } from '@/components/dashboard/action';
 import MovieTable from '@/components/dashboard/components/movie-table/movie-table';
 import { getData } from '@/googleDrive';
 import { IMovie } from '@/models/movie/movie';
+import React from 'react';
 
-import React from 'react'
+export const revalidate = 60;
 
-const Page = async () => {
+export async function getDataPage() {
   const { movies } = await getData() as {movies: IMovie[]}
   const { movieInDb } = await getAllMovies()
- 
+
+  return {movies, movieInDb}
+}
+
+const Page = async () => {
+    const {movies, movieInDb} = await getDataPage()
+
     return <MovieTable movies={movies} movieInDb={movieInDb as any}  />
 }
 
--- src/app/dashboard/(dashboard)/favorite/page.tsx
+++ src/app/dashboard/(dashboard)/favorite/page.tsx
@@ -5,10 +5,17 @@ import { getFavoriteMovies } from '@/components/dashboard/action';
 import Title from '@/components/ui/components/title/title';
 import { IMovie } from '@/models/movie/movie';
 import MoviesFavorite from '@/components/movies/components/movies-favorite/movies-favorite';
-const Page = async () => {
+
+export async function getData() {
   const session = await auth()
   //@ts-ignore
-  const   { movies }   = session?.user?.id &&  await getFavoriteMovies(session?.user?.id)
+  const   { movies } = session?.user?.id &&  await getFavoriteMovies(session?.user?.id)
+  return { movies}
+}
+
+const Page = async () => {
+
+  const { movies} = await getData()
   const moviesArray = movies?.map((movie: { movie: IMovie; }) => movie?.movie)
   return (
     <main className="flex flex-1 flex-col gap-4 md:gap-8 md:p-6">
