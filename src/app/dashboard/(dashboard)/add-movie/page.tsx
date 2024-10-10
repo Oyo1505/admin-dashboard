cd657a4d3d09f94a7bc6f72a09d8df285de0f43e
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2024-10-10 11:00:33 +0200

    cache validation

--- src/app/dashboard/(dashboard)/add-movie/page.tsx
+++ src/app/dashboard/(dashboard)/add-movie/page.tsx
@@ -1,20 +1,20 @@
 import { getAllMovies } from '@/components/dashboard/action';
 import MovieTable from '@/components/dashboard/components/movie-table/movie-table';
-import { getData } from '@/googleDrive';
+import { getDataFromGoogleDrive } from '@/googleDrive';
 import { IMovie } from '@/models/movie/movie';
 import React from 'react';
 
 export const revalidate = 60;
 
-export async function getDataPage() {
-  const { movies } = await getData() as {movies: IMovie[]}
+export async function getData() {
+  const { movies } = await getDataFromGoogleDrive() as {movies: IMovie[]}
   const { movieInDb } = await getAllMovies()
 
   return {movies, movieInDb}
 }
 
 const Page = async () => {
-    const {movies, movieInDb} = await getDataPage()
+    const {movies, movieInDb} = await getData()
 
     return <MovieTable movies={movies} movieInDb={movieInDb as any}  />
 }
