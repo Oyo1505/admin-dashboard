180a7465d1b6de223787fa32fa7c352e44eb7b08
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2024-12-12 10:13:47 +0100

    refqct

--- src/components/dashboard/components/button-add-movie/button-add-movie.tsx
+++ src/components/dashboard/components/button-add-movie/button-add-movie.tsx
@@ -9,75 +9,54 @@ import React from 'react'
 import { useForm, Controller } from 'react-hook-form';
 import { useMutation } from '@tanstack/react-query'
 
+interface FormValues {
+  file: File;
+}
+
 const ButtonAddMovie = () => {
-
-  // const [canBeUploaded, setCanBeUploaded] = React.useState(false);
-  // const [fileName , setFileName] = useState('');
-  // const [file , setFile] = useState<File>();
-  
-  // const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   if(event.target.files && event.target.files.length > 0){
-  //   const fileUploaded = event.target.files[0];
-  //   if(fileUploaded.size > 0 && fileUploaded.type === 'video/mp4'){
-  //       setCanBeUploaded(true);
-  //       setFileName(fileUploaded.name);
-  //       setFile(fileUploaded)
-  //     }
-  //   }
-  // };
-  const { mutate, isPending } =  useMutation({
-    mutationFn: (data:any) => addFileToGoogleDriveAction(data.file),
+  const { mutate, isPending } = useMutation({
+    mutationFn: (data: FormValues) => addFileToGoogleDriveAction(data.file),
   });
-  
-  // useEffect(() => {
-  //   if( fileName){
-  //  //   setFileName('');
-  //   }
-  //   if( !fileName){
-  //     setCanBeUploaded(false);
-  //   }
-  // }, [isPending, fileName]);
-  
-  const { handleSubmit, control, } = useForm({
-   resolver: zodResolver(movieUploadSchema)
-  },
-);
-const  onSubmit = async (data : any) => {
-  try {
-    mutate(data)
-  } catch (error) {
-    console.log(error)
-  } 
-}
+
+  const { handleSubmit, control } = useForm<FormValues>({
+    resolver: zodResolver(movieUploadSchema)
+  });
+
+  const onSubmit = async (data: FormValues) => {
+    try {
+      mutate(data);
+    } catch (error) {
+      console.error('Erreur lors du téléchargement:', error);
+    }
+  };
+
+  if (isPending) {
+    return <LoadingSpinner />;
+  }
+
   return (
-    <div>
-      {isPending ? <LoadingSpinner /> :  
-     <form onSubmit={handleSubmit(onSubmit)}>
-     <Controller
-       name="file"
-       control={control}
-       render={({  field: { onChange }  }) => (
-         <Input
-           type="file"
-           accept=".mp4"
-           onChange={(e)=>e.target.files && onChange(e.target.files[0]) }
-         />
-       )}
-     />
-{/* 
-     {canBeUploaded && 
-      <>
-        <div>{fileName}</div>
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <Controller
+        name="file"
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Input
+            type="file"
+            accept=".mp4"
+            onChange={(e) => {
+              const files = e.target.files;
+              if (files?.length) {
+                onChange(files[0]);
+              }
+            }}
+          />
+        )}
+      />
+      <Button type="submit" variant="outline">
+        Soumettre
+      </Button>
+    </form>
+  );
+};
 
-      </>
-      
-     } */}
-
-<Button  type='submit' variant='outline' >Submit</Button>
-   </form>
-      }
-    </div>
-  )
-}
-
-export default ButtonAddMovie
+export default ButtonAddMovie;
