29e354284449f86a6bd3d4236939ed5f12464c17
Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2026-01-14 12:37:30 +0100

    fixed

--- src/domains/movies/components/movie-player-iframe/movie-player-iframe.tsx
+++ src/domains/movies/components/movie-player-iframe/movie-player-iframe.tsx
@@ -3,13 +3,14 @@ import { updateAnalyticsLastMovieWatched } from '@/domains/auth/actions/action.a
 import { useSession } from '@/lib/auth-client';
 import { isValidIframeUrl, sanitizeGoogleDriveUrl } from '@/lib/security';
 import { IMovie } from '@/models/movie/movie';
+import { useState } from 'react';
 
 import Iframe from 'react-iframe';
 
 const MoviePlayerIframe = ({ movie }: { movie: IMovie }) => {
   const { data: session } = useSession();
   const iframeUrl = sanitizeGoogleDriveUrl(movie?.idGoogleDive || '');
-
+  const [hasTracked, setHasTracked] = useState(false);
   if (!isValidIframeUrl(iframeUrl)) {
     return (
       <div className="w-full md:h-[400px] lg:w-full h-[250px] lg:h-[450px] flex items-center justify-center bg-gray-100 rounded-lg">
@@ -21,7 +22,7 @@ const MoviePlayerIframe = ({ movie }: { movie: IMovie }) => {
   return (
     <Iframe
       url={iframeUrl}
-      className="w-full md:h-[400px] lg:w-full h-[250px] lg:h-[450px]"
+      className="w-full md:h-100 lg:w-full h-62.5 lg:h-112.5 "
       width="auto"
       title={movie?.title}
       height="450px"
@@ -29,8 +30,10 @@ const MoviePlayerIframe = ({ movie }: { movie: IMovie }) => {
       importance="high"
       ariaLabel="video player"
       onLoad={() => {
-        if (session?.user?.id)
+        if (session?.user?.id && !hasTracked) {
+          setHasTracked(true);
           updateAnalyticsLastMovieWatched(session.user.id, movie.title);
+        }
       }}
     />
   );
